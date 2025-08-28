5dd1cbf0749419b13aa895aba8126466c05cb18e
sigvardbratlie <sigvard.bratlie@gmail.com>
2025-08-28 16:07:45 +0200

    first commit

---
+++ src/sibr_api.py
@@ -0,0 +1,394 @@
+import os
+import logging
+from http.client import responses
+import aiohttp
+import asyncio
+import inspect
+import pandas as pd
+from urllib.parse import quote_plus
+from typing import Literal
+import json
+import abc
+from dotenv import load_dotenv
+#os.chdir("..")
+load_dotenv()
+
+
+class RateLimitError(Exception):
+    """Custom exception for rate limit errors."""
+
+    def __init__(self, message="Rate limit exceeded. Please try again later."):
+        self.message = message
+        super().__init__(self.message)
+class APIkeyError(Exception):
+    """Custom exception for API key errors."""
+
+    def __init__(self, message="API key is invalid or missing."):
+        self.message = message
+        super().__init__(self.message)
+
+class ApiBase(metaclass=abc.ABCMeta):
+    """
+    A class to simplify async API requests.
+    """
+    def __init__(self,logger_name = 'ApiBase',logger = None, proxies : dict = None):
+        """
+
+        Args:
+            logger_name (str): A name for the logger. Default 'ApiBase' if no name is given.
+            logger (logging): A logger object. If left open, a default logger will be assigned.
+            proxies (dict): A dictionary with proxies. Defaults to None. Example: proxies = {'http' : proxy_url_http, 'https' : proxy_url_https}
+
+        """
+        if logger is None:
+            logger = logging.getLogger(logger_name)
+        self.logger = logger
+        self.api_key = None
+        self.session = None
+        self.use_proxy = True
+        self.proxies = proxies
+        self.base_url = None
+        self.ok_responses = 0
+        self.fail_responses = 0
+
+
+    def _ensure_fieldnames(self, df : pd.DataFrame) -> None:
+        '''
+        A function to ensure correct fieldnames of a pandas dataframe.
+        The function changes the field names inplace.
+
+        Args:
+            df (pd.DataFrame): The dataframe you wish to ensure fieldnames on
+
+        Returns:
+            None
+        '''
+        new_cols = []
+        for col in df.columns:
+            new_cols.append(col.replace('.', '_', ))
+        df.columns = new_cols
+
+    def _mk_proxy(self, url: str) -> str:
+        '''
+        A function to choose the right proxy from the proxies dictionary.
+
+        Args:
+            url (str): The endpoint url in use
+
+        Returns:
+            proxy_url (str): The proxy url to use
+        '''
+        proxy_url = None
+        if self.use_proxy:
+            if url.startswith('https://'):
+                proxy_url = self.proxies['https']
+            else:
+                proxy_url = self.proxies['http']
+        return proxy_url
+
+    async def _ensure_session(self) -> None:
+        '''
+        A function to ensure the session is active and running.
+        The function updates the class attribute `.session` when called.
+
+        Returns:
+            None
+        '''
+        if self.session is None or self.session.closed:
+            self.session = aiohttp.ClientSession()
+
+    async def close(self) -> None:
+        '''
+        A function to close the session.
+
+        Returns:
+            None
+        '''
+        if self.session and not self.session.closed:
+            await self.session.close()
+
+    async def _reset_session(self):
+        '''
+        A function to reset the session.
+
+        Returns:
+            None
+        '''
+        if self.session:
+            await self.session.close()
+        self.session = None
+        return await self._ensure_session()
+
+    async def fetch_single(self, url : str,headers : dict = None,proxy_url : str = None) -> dict:
+        '''
+        Async method to fetch a given url.
+
+        Args:
+            url (str): The url to fetch
+            headers (dict): Header. Example {'User-Agent': 'YourApp/1.0'}
+            proxy_url (str): The proxy url to use
+
+        Returns:
+            dict: The JSON response from the API call.
+
+        Raises:
+            RateLimitError: If the rate limit is exceeded.
+            APIkeyError: If the API key is invalid or missing.
+        '''
+        await self._ensure_session()
+        try:
+            async with self.session.get(url, params=headers, proxy=proxy_url) as response:
+                if response.status == 429:
+                    error_message = await response.text()
+                    raise RateLimitError(
+                        f'Rate limit exceeded. Error: {error_message}')
+                if response.status == 403:
+                    error_message = await response.text()
+                    raise PermissionError(f'Permission denied. Error: {error_message}')
+                if response.status == 401:
+                    error_message = await response.text()
+                    raise APIkeyError(f'Authorization error. Error: {error_message}')
+                if response.status == 200:
+                    response = await response.json()
+                    return response
+                else:
+                    error_text = await response.text()
+                    self.logger.error(
+                        f'Error message - {inspect.currentframe().f_code.co_name}: {response.status}, {error_text}.')
+                    return None
+        except RateLimitError:
+            raise
+        except APIkeyError:
+            raise
+        except aiohttp.ClientError as e:
+            self.logger.error(f"network failure - {e}. url {url}")
+            return None
+        except asyncio.TimeoutError:
+            self.logger.error(f"Timeout error.")
+            return None
+        except Exception as e:
+            self.logger.error(
+                f"Unexpected error in fetch_single - {e}")
+            print(f'Reponse: {response}, response type : {type(response)}, url {url} ')
+            return None
+
+    @abc.abstractmethod
+    def transform_output(self,output):
+        """
+        Abstract method to transform the raw API response into a desired format.
+
+        This method should be implemented by subclasses to parse and
+        restructure the data received from an API call.
+
+        """
+        pass
+
+    @abc.abstractmethod
+    def get_item(self, item):
+        """
+        Abstract method to fetch a single item from the API.
+
+        This method should be implemented by subclasses to define how a
+        single request is made to the specific API.
+
+        NB: This function must be an async method and use the `fetch_single` class method to get the desired url.
+
+        Args:
+            item: The input parameter(s) required to fetch a single item.
+                  The type and content of 'item' will depend on the specific API.
+        """
+        pass
+
+    @abc.abstractmethod
+    def save_func(self,results : list):
+        """
+        Abstract method to save the processed results.
+
+        This method should be implemented by subclasses to define how the
+        accumulated results from API calls are persisted (e.g., to a file,
+        database, or other storage).
+        Args:
+            results (list): A list of processed results to be saved.
+        """
+        pass
+
+    async def _process_batch(self,tasks : list):
+        """
+        A function to process a batch of tasks.
+
+        Args:
+            tasks (list): The list of tasks to be processed
+
+        Yields:
+            result: The result of the task
+        """
+
+        for future in asyncio.as_completed(tasks):
+            result = await future
+            if isinstance(result,RateLimitError):
+                self.logger.warning(f'Rate limit exceeded. Stopping code. Please try again later.')
+                break
+            yield result
+
+    async def _process_tasks(self,tasks : list, save : bool, save_interval : int) -> list:
+        """
+        A function to process a list of tasks.
+
+        Args:
+            tasks (list): The list of tasks to be processed
+            save (bool): A boolean parameter to decide to save the data or not.
+            save_interval (int): The interval at which to save the data.
+
+        Returns:
+            all_results (list): The list of all results.
+        """
+
+        all_results = []
+        results_to_save = []
+
+        processed_results = self._process_batch(tasks)
+        count = 0
+        try:
+            async for result in processed_results:
+                count += 1
+                if result is None:
+                    self.logger.warning(f'Results is None')
+                    continue
+
+                results_to_save.append(result)
+                if count % 500 == 0:
+                    self.logger.info(f'Processed {count} so far. Successful requests: {self.ok_responses} | failed requests {self.fail_responses}')
+
+                if len(results_to_save) >= save_interval:
+                    self.logger.info(f'Processed {count} so far. Save interval of {save_interval} reached.')
+                    if save:
+                        self.logger.info(f'Saving {len(results_to_save)} results')
+                        self.save_func(results_to_save)
+                    all_results.extend(results_to_save)
+                    results_to_save.clear()
+
+        except RateLimitError:
+            self.logger.warning(f'Rate limit exceeded. Stopping code. Please try again later.')
+        finally:
+            if results_to_save:
+                if save:
+                    self.save_func(results_to_save)
+                all_results.extend(results_to_save)
+                results_to_save.clear()
+        self.logger.info(f'Geocoding job finished. Successful requests: {self.ok_responses} | failed requests {self.fail_responses}')
+        return all_results
+
+    async def get_items_with_ids(self,
+                               inputs : list | dict,
+                               save: bool = False,
+                               save_interval: int = 50000,
+                               concurrent_requests : int = 5,
+                               ) -> list:
+        """
+        Fetches multiple items concurrently, associating each result with a unique ID.
+
+        This function is designed to handle a list or dictionary of inputs, where each input
+        can be uniquely identified. It uses asyncio.Semaphore to limit the number of
+        concurrent requests to prevent overwhelming the API.
+
+        Args:
+            inputs (list | dict): A list of items or a dictionary where keys are item IDs
+                                   and values are the items to be fetched.
+            save (bool): If True, results will be saved periodically using `_save_func`.
+            save_interval (int): The number of results to accumulate before saving (if `save` is True).
+            concurrent_requests (int): The maximum number of concurrent API requests.
+
+        Returns:
+            list: A list of tuples, where each tuple contains (item_id, result).
+                  `result` will be None if an error occurred during fetching for that item.
+
+        Raises:
+            TypeError: If `inputs` is not a list or a dictionary.
+            RateLimitError: Propagated from `_fetch_single` if a rate limit is encountered.
+        """
+
+
+        if isinstance(inputs, list):
+            inputs = {addr : addr for addr in inputs}
+        elif isinstance(inputs, dict):
+            pass
+        else:
+            raise TypeError(f'Invalid input type. Expected list or dict, but got {type(inputs)}')
+
+        semaphore = asyncio.Semaphore(concurrent_requests)
+
+        async def fetch_item_with_id(item_id, item):
+            """
+            Takes in both an item_id and address.
+            Args:
+                item_id (str):
+                address (str):
+
+            Returns (tuple): (item_id, result)
+
+            """
+            async with semaphore:
+                try:
+                    result  = await self.get_item(item)
+                    return (item_id, result)
+                except RateLimitError:
+                    self.logger.warning(f'Rate limit exceeded. Stopping code. Please try again later.')
+                    raise
+                except Exception as e:
+                    self.logger.error(f'Error fetching item {item} with item_id {item_id} - {e}')
+                    return (item_id, None)
+
+        tasks = [fetch_item_with_id(item_id=item_id, item=item) for item_id,item in inputs.items()]
+
+        all_results = await self._process_tasks(tasks, save, save_interval)
+        return all_results
+
+    async def get_items(self,
+                               inputs : list | dict,
+                               save: bool = False,
+                               save_interval: int = 50000,
+                               concurrent_requests : int = 5,
+                               ) -> list:
+        """
+        Fetches multiple items concurrently.
+
+        This function is designed to handle a list of inputs. It uses asyncio.Semaphore
+        to limit the number of concurrent requests to prevent overwhelming the API.
+
+        Args:
+            inputs (list): A list of items to be fetched.
+            save (bool): If True, results will be saved periodically using `_save_func`.
+            save_interval (int): The number of results to accumulate before saving (if `save` is True).
+            concurrent_requests (int): The maximum number of concurrent API requests.
+
+        Returns:
+            list: A list of results. `None` will be included in the list if an error
+                  occurred during fetching for that item.
+
+        Raises:
+            TypeError: If `inputs` is not a list.
+            RateLimitError: Propagated from `_fetch_single` if a rate limit is encountered.
+        """
+
+
+        if not isinstance(inputs, list):
+            raise TypeError(f'Invalid input type. Expected list, but got {type(inputs)}')
+
+        semaphore = asyncio.Semaphore(concurrent_requests)
+
+        async def fetch_item(item):
+            async with semaphore:
+                try:
+                    result  = await self.get_item(item)
+                    return result
+                except RateLimitError:
+                    self.logger.warning(f'Rate limit exceeded. Stopping code. Please try again later.')
+                    raise
+                except Exception as e:
+                    self.logger.error(f'Error fetching item {item} with - {e}')
+                    return None
+
+        tasks = [fetch_item(item=item) for item_id,item in inputs]
+
+        all_results = await self._process_tasks(tasks, save, save_interval)
+        return all_results
